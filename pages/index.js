88799582fd2f8d9cf582bae520b5cd38397744bd
Guilherme Tiago <gui-kuunzler@hotmail.com>
2021-03-21 18:40:20 -0300

    teste 2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,6 @@
-import { useState } from 'react';
 
 function Home(){
-    return (
-        <div>
-            <h3>Home</h3>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
+    return <h1>Teste</h1>
 }
 
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 export default Home
